fdce40fbdf19ad5d704ce678a5caf33e9693bcc3
felipebenavides-maker <felipe.benavides@utp.edu.co>
2025-12-01 20:42:07 -0500

    Create PagesDashboard component for user dashboard

---
+++ SRC/Modules/PagesDashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useAuth } from "../../../context/AuthContext";
+
+export default function Dashboard() {
+  const { user } = useAuth();
+
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <p>Bienvenido <b>{user?.username}</b></p>
+    </div>
+  );
+}
