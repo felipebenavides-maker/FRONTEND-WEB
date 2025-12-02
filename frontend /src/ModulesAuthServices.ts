1c1e75810aeb65fe64d974d6a228362d81a3d5e7
felipebenavides-maker <felipe.benavides@utp.edu.co>
2025-12-01 20:28:21 -0500

    Delete frontend directory

--- frontend /src/ModulesAuthServices.ts
+++
@@ -1,8 +0,0 @@
-import api from "../../api/api";
-import { LoginPayload, RegisterPayload } from "./types";
-
-export const loginService = (data: LoginPayload) =>
-  api.post("/api/v1/auth/login", data);
-
-export const registerService = (data: RegisterPayload) =>
-  api.post("/api/v1/auth/register", data);
